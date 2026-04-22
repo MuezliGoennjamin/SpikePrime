937811a6d57c30b0596268b8968e8d04ab1caecb
MuezliGoennjamin <157050185+MuezliGoennjamin@users.noreply.github.com>
2026-04-22 10:36:06 +0200

    Achsen Spielraum Test

---
+++ axis_test.py
@@ -0,0 +1,29 @@
+import runloop
+import motor, distance_sensor, color_sensor, color
+from hub import port, button
+
+# Programm auf kleines Feld agestimmt
+
+# Hub Anschluss: Motor X1 = Port A, Motor X2 = Port B, Motor Y2 = Port C, Motor Z2 = Port D, Distance Sensor = Port E, Color Sensor = Port F
+
+#global variables
+velocity_X1 = 90
+velocity_X2 = 90
+velocity_Y2 = 50
+velocity_Z2 = 10
+Motor_X1 = port.A           # Hub Port A
+Motor_X2 = port.B           # Hub Port B
+Motor_Y2 = port.C           # Hub Port C
+Motor_Z2 = port.D           # Hub Port D
+height_tablet = 15          # [mm]
+
+
+async def main():
+
+
+    await motor.run_for_degrees(Motor_X1, 900, velocity_X1, stop=motor.CONTINUE)
+    await motor.run_for_degrees(Motor_X2, 400, velocity_X2, stop=motor.CONTINUE)
+    await motor.run_for_degrees(Motor_Y2, 400, velocity_Y2, stop=motor.CONTINUE)
+    await motor.run_for_degrees(Motor_Z2, 100, velocity_Z2, stop=motor.CONTINUE)
+
+runloop.run(main())
