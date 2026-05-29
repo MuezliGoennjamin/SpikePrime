ce9886cbd6173614ba80456ba38cca8e519c35c1
MuezliGoennjamin <157050185+MuezliGoennjamin@users.noreply.github.com>
2026-05-29 15:07:28 +0200

    Update axis_test.py

--- axis_test.py
+++ axis_test.py
@@ -1,38 +1,14 @@
-from pybricks.parameters import Port, Color
-from pybricks.pupdevices import ColorSensor
-from pybricks.tools import wait, multitask, run_task
+from pybricks.pupdevices import ColorSensor, ForceSensor
+from pybricks.parameters import Port
 
-# Sensor initialisieren
-sensor = ColorSensor(Port.D)
+button = ForceSensor(Port.B)
+sensor = ColorSensor(Port.F)
 
-# --------------------------------------------------
-# Hintergrund-Task: Licht dauerhaft setzen
-# --------------------------------------------------
-async def light_task():
-    while True:
-        # konstantes Licht (grün hier als Beispiel)
-        sensor.lights.on([0, 20, 0])
-        await wait(100)
-
-# --------------------------------------------------
-# Haupt-Task: Farbe + Helligkeit ausgeben
-# --------------------------------------------------
-
-
-# --------------------------------------------------
-# Main
-# --------------------------------------------------
-async def main():
-    await multitask(
-        light_task()
-)
-
-    print("Farbe:", sensor.color())
-    print("Reflex:", sensor.reflection())
-    print("----------------------")
-    await wait(500)
-# Start
-run_task(main())
+while True:
+    if button.pressed():
+        hsv = sensor.hsv(surface=False)
+        ambient = sensor.ambient()
+        print(hsv, ambient)
 
 
 
